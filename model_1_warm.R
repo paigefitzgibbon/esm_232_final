f4dc1f9d8d1bc19bbf7d61fb07ed78d586978458
Mario Colon <mariocolon@ucsb.edu>
2019-06-01 16:17:42 -0700

    For eric

---
+++ model_1_warm.R
@@ -0,0 +1,15 @@
+#' @title Snowmelt Model - Warming 
+#' 
+#' @param ci = heat capacity 
+#' @param pw = melting point
+#' @param hm = milimeter of snow
+#' @param Ts = snowpack temp 
+#' @param Tm = melting point of snow 
+#' 
+#' 
+#' @author Mario Colon & Eric Holmes 
+#' 
+
+
+Qcc = -ci
+.ρw.hm.(Ts-Tm) 
