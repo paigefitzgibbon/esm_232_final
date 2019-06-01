9bc102766164dc90fe556b3b4514aadb7153a64f
Mario Colon <mariocolon@ucsb.edu>
2019-06-01 16:29:17 -0700

    New Model (ripe)

---
+++ model_2_ripe.R
@@ -0,0 +1,10 @@
+#' #' @title Snowmelt Model - Ripening  
+#' 
+#' @param ci = heat capacity (constant)
+#' @param pw = density of water (constant)
+#' @param hm = milimeter of snow (input)
+#' @param Ts = snowpack temp (input)
+#' @param 
+#' 
+#' @author Mario Colon & Eric Holmes 
+#' 
