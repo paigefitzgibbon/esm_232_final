6fa9c21192d48917b8b6242e5ddb8193b742dea1
Mario Colon <mariocolon@ucsb.edu>
2019-06-01 17:09:22 -0700

    Model 3

---
+++ model_3_output.R
@@ -0,0 +1,16 @@
+#' #' @title Snowmelt Model - Output  
+#' 
+#' 
+#' @param θret = liquid water content (constnat), typical = 0.03
+#' @param hs = snow depth 
+#' @param pw = density of water (constant), = 1000 kg m-3
+#' @param λf = latent heat of fusion (0.334 MJ kg-1)
+#' 
+#' 
+#' @return Ripe i.e. pores filled 
+#' 
+#' @author Mario Colon & Eric Holmes 
+#' 
+
+
+Qm3 = (hm – hwret).ρw.λf
